1e17a9ec2a9b1cb2dad1f17bbcd7724732a2790d
Eugen Istoc <eugenistoc@gmail.com>
2018-04-13 17:36:29 -0400

    Initial commit

---
+++ desktop/src/renderer/App.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  div#app
+    pre {{crypt.genSaltSync(10)}}
+    router-view
+
+</template>
+
+<script>
+import bcrypt from "bcryptjs";
+
+export default {
+  name: "helper",
+  data() {
+    return {
+      crypt: bcrypt
+    };
+  }
+};
+</script>
+
+<style>
+/* CSS */
+</style>
